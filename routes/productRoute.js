59334b1d47147632dfd92cc0420d6d925acde304
garfieldvans <iyanhalo@gmail.com>
2024-01-05 18:41:27 +0700

    vercel setting

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,7 +3,7 @@ const Product = require("../models/shoesModels");
 const routes = express.Router()
 
 // create new product
-routes.post("/", async (req, res) => {
+routes.post("/new", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
