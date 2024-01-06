50fc17ad6eb933ca0c681e59ebdf6a26cd8e9fb8
garfieldvans <iyanhalo@gmail.com>
2024-01-07 01:00:42 +0700

    search limit

--- routes/search.js
+++ routes/search.js
@@ -8,49 +8,30 @@ searchroutes.use(cors());
 
 searchroutes.get("/q", async (req, res) => {
     try {
-      const query = req.query.query;
-  
-      const page = parseInt(req.query.page) || 1;
-      const pageSize = parseInt(req.query.pageSize) || 10;
-  
-      const startIndex = (page - 1) * pageSize;
-  
-      // Use MongoDB $regex operator for case-insensitive search
-      const results = await Product.find({
-        $or: [
-          { name: { $regex: new RegExp(query, 'i') } },
-          { description: { $regex: new RegExp(query, 'i') } }
-        ]
-      }).skip(startIndex).limit(pageSize);
-  
-      const totalItems = await Product.countDocuments({
-        $or: [
-          { name: { $regex: new RegExp(query, 'i') } },
-          { description: { $regex: new RegExp(query, 'i') } }
-        ]
-      });
+        const query = req.query.query;
+    
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.pageSize) || 10;
+    
+        const startIndex = (page - 1) * pageSize;
+    
+        // Use MongoDB $regex operator for case-insensitive search
+        const results = await Product.find({
+          $or: [
+            { name: { $regex: new RegExp(query, 'i') } },
+            { description: { $regex: new RegExp(query, 'i') } }
+          ]
+        }).skip(startIndex).limit(pageSize);
     
         if (results.length === 0) {
           return res.json({ message: `Product with name ${query} not found` });
         }
-
-        const totalPages = Math.ceil(totalItems / pageSize);
-
-    if (results.length === 0) {
-      return res.json({ message: `Product with name ${query} not found` });
-    }
     
-        res.json({
-            results,
-            pagination: {
-              currentPage: page,
-              totalPages,
-              totalItems
-            }
-          });
+        res.json(results);
       } catch (error) {
         res.status(500).json({ message: error.message });
       }
     });
+
 
 module.exports = searchroutes;
