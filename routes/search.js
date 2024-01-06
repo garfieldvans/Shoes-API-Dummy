95274b43979335df75aebd96ab9443d24036081a
garfieldvans <iyanhalo@gmail.com>
2024-01-07 00:42:53 +0700

    search limit

--- routes/search.js
+++ routes/search.js
@@ -7,23 +7,30 @@ const cors = require("cors");
 searchroutes.use(cors());
 
 searchroutes.get("/q", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    const query = req.query.query;
-    const results = products.filter(
-      (item) =>
-        item.name.toLowerCase().includes(query.toLowerCase()) ||
-        item.description.toLowerCase().includes(query.toLowerCase())
-    );
-    if (results.length === 0) {
-        return res.json({ message: `Product with name ${query} not found` });
-      }
-    res.json(results);
-
+    try {
+        const query = req.query.query;
     
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = parseInt(req.query.pageSize) || 10;
+    
+        const startIndex = (page - 1) * pageSize;
+    
+        // Use MongoDB $regex operator for case-insensitive search
+        const results = await Product.find({
+          $or: [
+            { name: { $regex: new RegExp(query, 'i') } },
+            { description: { $regex: new RegExp(query, 'i') } }
+          ]
+        }).skip(startIndex).limit(pageSize);
+    
+        if (results.length === 0) {
+          return res.json({ message: `Product with name ${query} not found` });
+        }
+    
+        res.json(results);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
 
 module.exports = searchroutes;
